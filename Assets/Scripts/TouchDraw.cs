9932687b50087f6aa3d17073a0376a301cfa3c31
rosekelly6400 <109883728+rosekelly6400@users.noreply.github.com>
2025-10-04 14:50:46 -0400

    working gesture recognition

--- Assets/Scripts/TouchDraw.cs
+++ Assets/Scripts/TouchDraw.cs
@@ -40,7 +40,7 @@ public class TouchDraw : MonoBehaviour
     bool checkStraight()
     {
         bool isStraight = true;
-        float averageSlope = (end.y - start.y) / (end.x - start.x);
+        double averageSlope = (end.y - start.y) / (end.x - start.x);
 
         Vector3[] linePoints = new Vector3[line.positionCount];
         line.GetPositions(linePoints);
@@ -48,24 +48,113 @@ public class TouchDraw : MonoBehaviour
         {
             Vector3 p1 = linePoints[i];
             Vector3 p2 = linePoints[i + 3];
-            float currSlope = (p2.y - p1.y) / (p2.x - p1.x);
-            if (Mathf.Abs((averageSlope - currSlope) / averageSlope) > .3)
+            double currSlope = (p2.y - p1.y) / (p2.x - p1.x);
+            if (System.Math.Abs((averageSlope - currSlope) / averageSlope) > 100)
             {
                 isStraight = false;
             }
         }
         return isStraight;
     }
+
+    double getSlope(double x1, double y1, double x2, double y2)
+    {
+        if (end.x == start.x)
+        {
+            if (end.y - start.y >= 0)
+            {
+                return Double.PositiveInfinity;
+            }
+            else
+            {
+                return Double.NegativeInfinity;
+            }
+        }
+
+        else
+        {
+            return (y2- y1) / (x2- x1);
+        }
+        //return (y2- y1) / (x2- x1);
+    }
+
+
+    string checkLineTypeSimplified()
+    {
+        string lineType = "INVALID";
+        double averageSlope = getSlope(start.x, start.y, end.x, end.y);
+        double upperSlopeBound = 0.0;
+        double lowerSlopeBound = 0.0;
+
+        // vertical line
+        if (averageSlope >= 8.0 || averageSlope <= -8.0)
+        {
+            upperSlopeBound = 7.0;
+            lowerSlopeBound = 7.0;
+            lineType = "VERTICAL";
+        }
+        //diagonal up
+        else if (averageSlope < 8.0 && averageSlope > 0.15)
+        {
+            upperSlopeBound = 10.0;
+            lowerSlopeBound = .02;
+            lineType = "DIAGONAL_UP";
+        }
+        // diagonal down
+        else if (averageSlope > -8.0 && averageSlope < -0.15)
+        {
+            upperSlopeBound = -.02;
+            lowerSlopeBound = -10.0;
+            lineType = "DIAGONAL_DOWN";
+        }
+        //horizontal
+        else
+        {
+            upperSlopeBound = .3;
+            lowerSlopeBound = -.3;
+            lineType = "HORIZONTAL";
+        }
+
+        Vector3[] linePoints = new Vector3[line.positionCount];
+        line.GetPositions(linePoints);
+        int sampleSize = linePoints.Length / 7;
+        Debug.Log("AVG SLOPE");
+        Debug.Log(averageSlope);
+        for (int i = 0; i < linePoints.Length - sampleSize; i += sampleSize)
+        {
+            Vector3 p1 = linePoints[i];
+            Vector3 p2 = linePoints[i + sampleSize];
+            double currSlope = getSlope(p1.x, p1.y, p2.x, p2.y);
+            Debug.Log(currSlope);
+            if (lineType == "VERTICAL" && System.Math.Abs(currSlope) < lowerSlopeBound)
+            {
+                Debug.Log("NOT straight ");
+                Debug.Log(p1);
+                Debug.Log(p2);
+                Debug.Log(currSlope);
+                lineType = "INVALID";
+            }
+            else if (lineType != "VERTICAL" && (currSlope > upperSlopeBound || currSlope < lowerSlopeBound))
+            {
+                Debug.Log("NOT straight ");
+                Debug.Log(p1);
+                Debug.Log(p2);
+                Debug.Log(currSlope);
+                lineType = "INVALID";
+            }
+        }
+        return lineType;
+    }
     void FinishLine()
     {
         StopCoroutine(drawing);
         end = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        bool isStraight = checkStraight();
+        string lineType = checkLineTypeSimplified();
         // bool isStraight = true;
         // Vector3[] linePoints = new Vector3[line.positionCount];
         // line.GetPositions(linePoints);
 
-        // // float avgSlope = -1;
+        // // double avgSlope = -1;
         // // if (linePoints.Length > 1)
         // // {
         // //     avgSlope = (linePoints[1].y - linePoints[0].y) / (linePoints[1].x - linePoints[0].x);
@@ -73,7 +162,7 @@ public class TouchDraw : MonoBehaviour
         // for (int i = 0; i < linePoints.Length - 7; i += 6)
         // {
 
-        //     // float newSlope = (linePoints[i + 1].y - linePoints[i].y) / (linePoints[i + 1].x - linePoints[i].x);
+        //     // double newSlope = (linePoints[i + 1].y - linePoints[i].y) / (linePoints[i + 1].x - linePoints[i].x);
         //     // if (Mathf.Abs((avgSlope - newSlope)/avgSlope) > .2)
         //     // {
         //     //     Debug.Log(Mathf.Abs((avgSlope - newSlope)/avgSlope));
@@ -89,26 +178,21 @@ public class TouchDraw : MonoBehaviour
 
         //     // Calculate the cross product magnitude
         //     // If points are collinear, the magnitude of the cross product will be zero
-        //     float crossProductMagnitude = Vector3.Cross(v1, v2).magnitude;
+        //     double crossProductMagnitude = Vector3.Cross(v1, v2).magnitude;
 
-        //     // Use a small tolerance for floating-point comparisons
+        //     // Use a small tolerance for doubleing-point comparisons
         //     if (isStraight && crossProductMagnitude > 0.8f) // Adjust tolerance as needed
         //     {
         //         Debug.Log(crossProductMagnitude);
         //         isStraight = false; // The line is not straight
         //     }
         // }
-        if (isStraight)
-        {
-            Debug.Log("STRAIGHT!! ");
-        }
-
-        float slope = (end.y - start.y) / (end.x - start.x);
-        if (slope < 0.5 && slope > -0.5)
-        {
-            // PUT EVENT CALLER HERE FOR HORIZONTAL
-            Debug.Log("HORIZONTAL!! ");
-        }
+        // if (isStraight)
+        // {
+        //     Debug.Log(lineType);
+        // }
+        Debug.Log(lineType);
+        
         Destroy(line);
     }
 

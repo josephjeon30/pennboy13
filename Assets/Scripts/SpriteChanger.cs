8bb68b787b1672df115c8d13075175c9daabeb16
xindiliu2 <xindi.berry@gmail.com>
2025-10-04 15:54:21 -0400

    create sprite that changes with key presses

---
+++ Assets/Scripts/SpriteChanger.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+
+public class SpriteChanger : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public SpriteRenderer spriteRenderer;
+    public Sprite defaultSprite;
+    public Sprite backSprite;
+    public Sprite forwardSprite;
+    public Sprite focusedSprite;
+    public Sprite shockSprite;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if ((spriteRenderer != null) && (defaultSprite != null)){
+            spriteRenderer.sprite = defaultSprite;
+        }
+    }
+
+    public void LeanBack()
+    {
+        if ((spriteRenderer != null) && (backSprite != null)){
+            spriteRenderer.sprite = backSprite;
+        }
+    }
+
+    public void LeanForward()
+    {
+        if ((spriteRenderer != null) && (forwardSprite != null)){
+            spriteRenderer.sprite = forwardSprite;
+        }
+    }
+
+    public void LoseFish()
+    {
+        if ((spriteRenderer != null) && (shockSprite != null)){
+            spriteRenderer.sprite = shockSprite;
+        }
+    }
+
+    public void Focus()
+    {
+        if ((spriteRenderer != null) && (focusedSprite != null)){
+            spriteRenderer.sprite = focusedSprite;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        // Method to change sprite (space key)
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Focus();
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            LoseFish();
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            LeanForward();
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            LeanBack();
+        }
+
+    }
+}
